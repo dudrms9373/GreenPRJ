b0d2f5291734adff874cc565d3b4c4507970068d
zerogeun <kyg_9373@naver.com>
2021-10-10 17:21:01 +0900

    커밋

---
+++ src/db/DAO.java
@@ -0,0 +1,37 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DAO {
+
+	public static void main(String[] args) {
+		String driver = "org.mariadb.jdbc.Driver";
+		String url = "jdbc:mariadb://3.34.43.108:3306/member";
+		String id = "user";
+		String pwd = "1234";
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, id, pwd);
+			if (conn != null) {
+				System.out.println("DB 접속 성공");
+			}
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로드 실패");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("DB 접속 실패");
+			e.printStackTrace();
+		}
+	}
+
+}
