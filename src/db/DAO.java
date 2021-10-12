bfff6cc396b08ffb5125e38ea8f005e2a555a759
zerogeun <55429152+dudrms9373@users.noreply.github.com>
2021-10-12 11:22:11 +0900

    Update DAO.java

--- src/db/DAO.java
+++ src/db/DAO.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 public class DAO {
 	
 		private String driver = "org.mariadb.jdbc.Driver";
-		private String url = "jdbc:mariadb://3.34.43.108:3306/member";
+		private String url = "jdbc:mariadb://3.34.43.108:3306/member"; // 현재 작동 안함. 나중에 프로젝트 시작하면 다시 만들어서 연결 하겠습니다.
 		private String id = "user";
 		private String pwd = "1234";
 
