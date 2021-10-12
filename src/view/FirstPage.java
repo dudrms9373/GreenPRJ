1f846001beedc70d9a8c24a1c132007cbc1e17f5
choi2864 <85035853+choi2864@users.noreply.github.com>
2021-10-12 14:16:13 +0900

    Update FirstPage.java

--- src/view/FirstPage.java
+++ src/view/FirstPage.java
@@ -1,4 +1,4 @@
-package view;
+package ProjectUI;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -6,36 +6,88 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JSlider;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class FirstPage extends JFrame{
+public class FirstPage extends JFrame implements ActionListener, MouseListener{
 	JPanel panel;
-	JButton btn;
+	JButton UserLoginButton, TrainerLoginButton;
 	public FirstPage() {
-		this.setTitle("연습용");
+		this.setTitle("첫 화면");
 		panel = new JPanel();
 		panel.setLayout(null);
 		
 		getContentPane().add(panel);
 		
-		JButton UserLoginButton = new JButton("회원");
+		UserLoginButton = new JButton("회원");
 		UserLoginButton.setBounds(23, 97, 97, 170);
 		panel.add(UserLoginButton);
 		
-		JButton TrainerLoginButton = new JButton("트레이너");
+		TrainerLoginButton = new JButton("트레이너");
 		TrainerLoginButton.setBounds(163, 97, 97, 170);
 		panel.add(TrainerLoginButton);
+		
+		this.UserLoginButton.addActionListener(this);
+		this.TrainerLoginButton.addActionListener(this);
+		
+		this.UserLoginButton.addMouseListener(this);
+		this.TrainerLoginButton.addMouseListener(this);
+		
+		
 		
 		this.setVisible(true);
 		this.setSize(300,400);
 		this.setLocationRelativeTo(null);
-		//this.setResizable(false);
+		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	public static void main(String[] args) {
 		new FirstPage();
 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch(e.getActionCommand()) {
+		case "회원"     : new UserLoginPage() ; break;
+		// case "트레이너" : -------------------------------- 트레이너 화면 입력하기!!
+		
+		}
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		this.dispose();
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
