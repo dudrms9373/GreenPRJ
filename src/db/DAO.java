6fc7ae455926832549737c5704aa42e3c9261b6b
seunggod <92019271+seunggod@users.noreply.github.com>
2021-10-13 17:15:09 +0900

    Update DAO.java

--- src/db/DAO.java
+++ src/db/DAO.java
@@ -18,6 +18,65 @@ public class DAO {
 	
 	}
 	
+	// CRUD ------------------------------------------------------------------------------------
+	//회원 정보 수정
+	public void updateMem
+			(String id, String name, String pwd ,
+					String tel,String addr, String speNote) {
+		String sql="UPDATE MEMBER AS M, SPECIAL AS S";
+		sql		+= " SET M.MEM_NAME= ?";
+		sql		+= " ,   M.TEL = ?";
+		sql		+= " ,   M.ADDR = ?";
+		sql		+= " ,   S.SPE_NOTE = ?";
+		sql		+= " WHERE M.ID= ?"
+				+ " AND M.ID = S.ID";
+		try {
+			conn=DBConn.getInstance();
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, name);
+			pstmt.setString(2, pwd);
+			pstmt.setString(3, tel);
+			pstmt.setString(4, addr);
+			pstmt.setString(5, speNote);
+			pstmt.executeUpdate();
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+	
+	//회원 정보 삭제
+	public void removeMem(String id) {
+		String sql="DELETE FROM MEMBER WHERE ID = ?";
+		
+		try {
+			conn=DBConn.getInstance();
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+	}
 	
 
 }
