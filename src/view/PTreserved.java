7121ce4009e7c35ccdf516e6cdba42fad36555c2
seunggod <92019271+seunggod@users.noreply.github.com>
2021-10-17 19:26:19 +0900

    버튼 초기 상태 설정  액션리스너 등록 actionPerformed구현(미완)

--- src/view/PTreserved.java
+++ src/view/PTreserved.java
@@ -51,60 +51,122 @@ public class PTreserved extends JFrame {
 		JBpreview.setBounds(12, 10, 79, 23);
 		getContentPane().add(JBpreview);
 		
-		JB9h = new JButton("AM 09:00");
+		JB9h = new JButton("09:00");
 		JB9h.setBounds(134, 77, 170, 60);
 		getContentPane().add(JB9h);
 
-		JB10h = new JButton("AM 10:00");
+		JB10h = new JButton("10:00");
 		JB10h.setBounds(313, 77, 170, 60);
 		getContentPane().add(JB10h);
 
-		JB12h = new JButton("PM 12:00");
+		JB12h = new JButton("11:00");
 		JB12h.setBounds(134, 136, 170, 60);
 		getContentPane().add(JB12h);
 
-		JB11h = new JButton("AM 11:00");
+		JB11h = new JButton("12:00");
 		JB11h.setBounds(313, 136, 170, 60);
 		getContentPane().add(JB11h);
 
-		JB13h = new JButton("PM 13:00");
+		JB13h = new JButton("13:00");
 		getContentPane().add(JB13h);
 		JB13h.setBounds(134, 209, 170, 60);
 
-		JB14h = new JButton("PM 14:00");
+		JB14h = new JButton("14:00");
 		getContentPane().add(JB14h);
 		JB14h.setBounds(313, 209, 170, 60);
 
-		JB18h = new JButton("PM 18:00");
+		JB18h = new JButton("18:00");
 		getContentPane().add(JB18h);
 		JB18h.setBounds(313, 327, 170, 60);
 
-		JB15h = new JButton("PM 15:00");
+		JB15h = new JButton("15:00");
 		getContentPane().add(JB15h);
 		JB15h.setBounds(134, 268, 170, 60);
 
-		JB19h = new JButton("PM 19:00");
+		JB19h = new JButton("19:00");
 		getContentPane().add(JB19h);
 		JB19h.setBounds(134, 386, 170, 60);
 
-		JB16h = new JButton("PM 16:00");
+		JB16h = new JButton("16:00");
 		getContentPane().add(JB16h);
 		JB16h.setBounds(313, 268, 170, 60);
 
-		JB20h = new JButton("PM 20:00");
+		JB20h = new JButton("20:00");
 		getContentPane().add(JB20h);
 		JB20h.setBounds(313, 386, 170, 60);
 
-		JB17h = new JButton("PM 17:00");
+		JB17h = new JButton("17:00");
 		getContentPane().add(JB17h);
 		JB17h.setBounds(134, 327, 170, 60);
 		
+		// 시간 버튼들을 ArrayList에 담음
+		ArrayList<JButton> btnSet = new ArrayList<JButton>();
+		btnSet.add(0, JB9h);
+		btnSet.add(1, JB10h);
+		btnSet.add(2, JB11h);
+		btnSet.add(3, JB12h);
+		btnSet.add(4, JB13h);
+		btnSet.add(5, JB14h);
+		btnSet.add(6, JB15h);
+		btnSet.add(7, JB16h);
+		btnSet.add(8, JB17h);
+		btnSet.add(8, JB18h);
+		btnSet.add(8, JB19h);
+		btnSet.add(8, JB20h);
+		
+		//default = 모두 예약 가능 상태
+		for (JButton jBtn : btnSet) {
+			jBtn.setBackground(Color.GREEN);
+		}
+		
+		
+		
+		
+		
+		// ActionListener에 버튼 등록
+		///이전 버튼 
+		this.JBpreview.addActionListener(this);;
+		///시간 버튼
+		this.JB9h.addActionListener(this);
+		this.JB10h.addActionListener(this);
+		this.JB11h.addActionListener(this);
+		this.JB12h.addActionListener(this);
+		this.JB13h.addActionListener(this);
+		this.JB14h.addActionListener(this);
+		this.JB15h.addActionListener(this);
+		this.JB16h.addActionListener(this);
+		this.JB17h.addActionListener(this);
+		this.JB18h.addActionListener(this);
+		this.JB19h.addActionListener(this);
+		this.JB20h.addActionListener(this);
 
 		this.setSize(600, 500);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 
 	}
+	
+	// ActionListener
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getActionCommand().equals("<이전")){ //이전 버튼
+			dispose(); new UserMenu();
+		}
+		String time="";
+		for (int num = 9; num <= 20; num++) {
+			time=String.format("%02d:00", num);
+			
+			if(e.getActionCommand().equals(time)){
+				String resDate="날짜 박스 getText() "+time; //수정 필요 (미완성)
+				System.out.println(resDate);
+			}
+			
+		}
+		
+		
+	}
+
 
 //	public static void main(String[] args) {
 //		new PTreserved();
