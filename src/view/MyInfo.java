9f4697dcb1bcf781e2bebd83dae08e8923d5968f
zerogeun <55429152+dudrms9373@users.noreply.github.com>
2021-10-18 12:19:59 +0900

    Update MyInfo.java

--- src/view/MyInfo.java
+++ src/view/MyInfo.java
@@ -1,24 +1,19 @@
-package view;
+public class MyInfo extends JFrame implements ActionListener {
 
-import java.awt.Color;
-import java.awt.Font;
+	JButton JbPreView, Jbfire, JbProlong, JbCancel;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+	JLabel lblMyInfo, lblName, lblTel, lblAddr, lblRemainDay, lblPtTime, lblMyName, lblMyTel, lblMyAddr, lblMyReaminDay,
+			lblMyPtTime, lblMyWeight, lblMyHeight, lblHeight, lblWeight;
 
-public class MyInfo extends JFrame {
+	private String id = "";
+	private String name = "";
 
-	JButton JbPreView, JbWidthDraw, JbProlong;
+	public MyInfo(String name, String id) {
 
-	JLabel lblMyInfo, lblName, lblTel, lblAddr, lblRemainDay, lblPtTime, 
-	       lblMyName, lblMyTel, lblMyAddr, lblMyReaminDay, lblMyPtTime;
-		
+		FitnessDao dao = new FitnessDao();
 
-	public MyInfo() {
+		this.id = id;
+		this.name = name;
 
 		this.setTitle("내 정보");
 
@@ -56,8 +51,8 @@ public class MyInfo extends JFrame {
 		lblAddr.setHorizontalAlignment(SwingConstants.CENTER);
 		lblAddr.setBounds(69, 159, 171, 29);
 
-		// 남은 일수 label
-		lblRemainDay = new JLabel("남은일수");
+		// 남은 횟수 label
+		lblRemainDay = new JLabel("남은횟수");
 		lblRemainDay.setHorizontalAlignment(SwingConstants.CENTER);
 		lblRemainDay.setFont(new Font("Dialog", Font.BOLD, 20));
 		lblRemainDay.setBounds(69, 293, 171, 29);
@@ -69,42 +64,59 @@ public class MyInfo extends JFrame {
 		lblPtTime.setBounds(69, 348, 171, 29);
 
 		// 회원탈퇴 버튼
-		JbWidthDraw = new JButton("회원탈퇴");
-		JbWidthDraw.setForeground(Color.BLACK);
-		JbWidthDraw.setBackground(Color.DARK_GRAY);
-		JbWidthDraw.setFont(new Font("굴림", Font.PLAIN, 10));
-		JbWidthDraw.setBounds(401, 405, 85, 23);
+		Jbfire = new JButton("회원탈퇴");
+		Jbfire.setForeground(Color.BLACK);
+		Jbfire.setBackground(Color.DARK_GRAY);
+		Jbfire.setFont(new Font("굴림", Font.PLAIN, 10));
+		Jbfire.setBounds(401, 405, 85, 23);
 
-		// 나의 남은 일 수
-		lblMyReaminDay = new JLabel("OO일");
-		lblMyReaminDay.setHorizontalAlignment(SwingConstants.CENTER);
-		lblMyReaminDay.setFont(new Font("Dialog", Font.PLAIN, 15));
-		lblMyReaminDay.setBounds(262, 293, 149, 32);
 
 		// 연장하기 버튼
 		JbProlong = new JButton("연장하기");
 		JbProlong.setBounds(382, 299, 85, 23);
 
+		
+		
+		// 데이터넣기이이이잉
+		
+		MemberVo mv = dao.getMemInfo(id);
+
+		String tel = mv.getTel();
+		String addr = mv.getAddress();
+		String remainNum = mv.getRemainNum();
+		String ptTime = mv.getPtTime();
+		String height = mv.getHeight();
+		String weight = mv.getWeight();
+
+		setLabel(name, tel, addr, remainNum, ptTime, height, weight);
+		
+
+		// 나의 남은 횟 수
+		// lblMyReaminDay = new JLabel("OO일");
+		lblMyReaminDay.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMyReaminDay.setFont(new Font("Dialog", Font.PLAIN, 15));
+		lblMyReaminDay.setBounds(262, 293, 149, 32);
+		
 		// 내 이름
-		lblMyName = new JLabel("OOO");
+		// lblMyName = new JLabel("OOO");
 		lblMyName.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMyName.setFont(new Font("Dialog", Font.PLAIN, 15));
 		lblMyName.setBounds(279, 87, 149, 32);
 
 		// 내 전화번호
-		lblMyTel = new JLabel("000-0000-0000");
+		// lblMyTel = new JLabel("000-0000-0000");
 		lblMyTel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMyTel.setFont(new Font("Dialog", Font.PLAIN, 15));
 		lblMyTel.setBounds(289, 121, 149, 32);
 
 		// 내 주소
-		lblMyAddr = new JLabel("부산광역시");
+		// lblMyAddr = new JLabel("부산광역시");
 		lblMyAddr.setFont(new Font("Dialog", Font.PLAIN, 15));
 		lblMyAddr.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMyAddr.setBounds(279, 159, 149, 32);
 
 		// 내 PT 예약시간
-		lblMyPtTime = new JLabel("2021-10-20 AM 00:00");
+		// lblMyPtTime = new JLabel("2021-10-20 AM 00:00");
 		lblMyPtTime.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMyPtTime.setFont(new Font("Dialog", Font.PLAIN, 15));
 		lblMyPtTime.setBounds(252, 348, 149, 32);
@@ -112,7 +124,7 @@ public class MyInfo extends JFrame {
 		// 버튼 및 Label 추가
 		getContentPane().add(JbPreView);
 		getContentPane().add(JbProlong);
-		getContentPane().add(JbWidthDraw);
+		getContentPane().add(Jbfire);
 
 		getContentPane().add(lblName);
 		getContentPane().add(lblTel);
@@ -127,33 +139,64 @@ public class MyInfo extends JFrame {
 		getContentPane().add(lblMyReaminDay);
 		getContentPane().add(lblMyTel);
 
-		JButton JbCancel = new JButton("예약변경");
+		JbCancel = new JButton("예약변경");
 		JbCancel.setBounds(401, 354, 85, 23);
 		getContentPane().add(JbCancel);
 
-		JLabel lblHeight = new JLabel("키");
+		lblHeight = new JLabel("키");
 		lblHeight.setFont(new Font("Dialog", Font.BOLD, 20));
 		lblHeight.setHorizontalAlignment(SwingConstants.CENTER);
 		lblHeight.setBounds(69, 202, 171, 29);
 		getContentPane().add(lblHeight);
 
-		JLabel lblWeight = new JLabel("몸무게");
+		lblWeight = new JLabel("몸무게");
 		lblWeight.setFont(new Font("Dialog", Font.BOLD, 20));
 		lblWeight.setHorizontalAlignment(SwingConstants.CENTER);
 		lblWeight.setBounds(69, 241, 171, 29);
 		getContentPane().add(lblWeight);
 
-		JLabel lblMyHeight = new JLabel("170");
+		// lblMyHeight = new JLabel("170");
 		lblMyHeight.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMyHeight.setFont(new Font("Dialog", Font.PLAIN, 15));
 		lblMyHeight.setBounds(279, 202, 149, 32);
 		getContentPane().add(lblMyHeight);
 
-		JLabel lblMyWeight = new JLabel("72");
+		// lblMyWeight = new JLabel("72");
 		lblMyWeight.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMyWeight.setFont(new Font("Dialog", Font.PLAIN, 15));
 		lblMyWeight.setBounds(279, 241, 149, 32);
 		getContentPane().add(lblMyWeight);
+
+		this.JbPreView.addActionListener(this);
+		this.JbProlong.addActionListener(this);
+		this.Jbfire.addActionListener(this);
+
+
+
+	}
+		public void actionPerformed(ActionEvent e) {
+		switch (e.getActionCommand()) {
+		case "<이전":
+			new UserMenu(name, id);
+			break;
+		case "회원탈퇴":
+			new UserFire(id);
+			break;
+		case "연장하기":
+			new PTPrice(id);
+			break;
+		}
+	}
+		public void setLabel(String name, String tel, String addr, String remainNum, String ptTime, String height,
+			String weight) {
+
+		lblMyName.setText(name);
+		lblMyAddr.setText(addr);
+		lblMyPtTime.setText(ptTime);
+		lblMyReaminDay.setText(remainNum);
+		lblMyTel.setText(tel);
+		lblMyHeight.setText(height);
+		lblMyWeight.setText(weight);
 
 	}
 	
