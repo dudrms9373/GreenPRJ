4244bf03be6d96f037f0f6cd02424da624e1e928
choi2864 <85035853+choi2864@users.noreply.github.com>
2021-10-18 16:16:02 +0900

    Update LoginPage.java

--- src/view/LoginPage.java
+++ src/view/LoginPage.java
@@ -19,7 +19,7 @@ public class LoginPage extends JFrame implements ActionListener{
 	private JTextField IDtextField;
 	JPasswordField PWField;
 	JButton memButton, traButton ,mLogin_Button, tLogin_Button,
-			JoinButton, IdSearchButton, PWSearchButton, BackButton;
+			JoinButton, IdSearchButton, PWSearchButton;
 	private String idcheck ;
 	
 	private FitnessDao fDao;
@@ -32,17 +32,19 @@ public class LoginPage extends JFrame implements ActionListener{
 		
 		
 		JLabel lblNewLabel = new JLabel("로그인");
-		lblNewLabel.setBounds(0, 22, 434, 27);
+		lblNewLabel.setBounds(127, 21, 266, 27);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("굴림", Font.BOLD, 23));
 		getContentPane().add(lblNewLabel);
 		
 		JLabel Label_ID = new JLabel("ID : ");
-		Label_ID.setBounds(36, 68, 57, 15);
+		Label_ID.setHorizontalAlignment(SwingConstants.CENTER);
+		Label_ID.setBounds(52, 85, 57, 15);
 		getContentPane().add(Label_ID);
 		
 		JLabel Label_PW = new JLabel("PW : ");
-		Label_PW.setBounds(36, 119, 57, 15);
+		Label_PW.setHorizontalAlignment(SwingConstants.CENTER);
+		Label_PW.setBounds(52, 150, 57, 15);
 		getContentPane().add(Label_PW);
 
 		//기관 소개
@@ -51,57 +53,51 @@ public class LoginPage extends JFrame implements ActionListener{
 		String info2="주소: OO시 OO동 OO로 01-23";
 		String info=String.format(fmt,info1,info2);
 		JLabel Label_Info = new JLabel(info);
-		Label_Info.setBounds(36, 250, 500, 50);
+		Label_Info.setBounds(33, 250, 377, 50);
 		getContentPane().add(Label_Info);
 		
 		
 		memButton = new JButton("회원");
 		//memButton.setBounds(260, 10, 81, 20);
-		memButton.setBounds(100, 10, 81, 20);
+		memButton.setBounds(94, 10, 97, 20);
 		getContentPane().add(memButton);
 		
 		traButton = new JButton("트레이너");
-		traButton.setBounds(250, 10, 97, 20);
+		traButton.setBounds(94, 39, 97, 20);
 		getContentPane().add(traButton);
 		
 		mLogin_Button = new JButton("로그인");
-		mLogin_Button.setBounds(16, 183, 81, 30);
+		mLogin_Button.setBounds(20, 210, 81, 30);
 		getContentPane().add(mLogin_Button);
 		
 		tLogin_Button = new JButton("로그인");
-		tLogin_Button.setBounds(120, 153, 200, 30);
+		tLogin_Button.setBounds(120, 210, 200, 30);
 		getContentPane().add(tLogin_Button);
 		this.tLogin_Button.setVisible(false);
 		
 		JoinButton = new JButton("회원가입");
-		JoinButton.setBounds(109, 183, 97, 30);
+		JoinButton.setBounds(110, 210, 97, 30);
 		getContentPane().add(JoinButton);
 		
 		IdSearchButton = new JButton("ID 찾기");		
-		IdSearchButton.setBounds(218, 183, 81, 30);
+		IdSearchButton.setBounds(220, 210, 81, 30);
 		getContentPane().add(IdSearchButton);
 		
 		PWSearchButton = new JButton("PW 찾기");
-		PWSearchButton.setBounds(311, 183, 97, 30);
+		PWSearchButton.setBounds(313, 210, 97, 30);
 		getContentPane().add(PWSearchButton);
 		
 		IDtextField = new JTextField();
 		IDtextField.setToolTipText("내용을 입력해주세요");
-		IDtextField.setBounds(125, 65, 219, 25);
+		IDtextField.setBounds(127, 80, 237, 25);
 		getContentPane().add(IDtextField);
 		IDtextField.setColumns(10);
 		
 		PWField = new JPasswordField();
 		PWField.setToolTipText("내용을 입력해주세요");
 		PWField.setColumns(10);
-		PWField.setBounds(125, 116, 219, 25);
+		PWField.setBounds(127, 145, 237, 25);
 		getContentPane().add(PWField);
-		
-		BackButton = new JButton("<이전");
-		BackButton.setBounds(16, 10, 80, 27);
-		getContentPane().add(BackButton);
-		
-		this.BackButton.addActionListener(this);
 		this.JoinButton.addActionListener(this);
 		this.IdSearchButton.addActionListener(this);
 		this.PWSearchButton.addActionListener(this);
@@ -112,7 +108,7 @@ public class LoginPage extends JFrame implements ActionListener{
 		
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(450,360);
+		this.setSize(450,340);
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
@@ -188,10 +184,11 @@ public class LoginPage extends JFrame implements ActionListener{
 		String id=this.IDtextField.getText();
 		String pwd=this.PWField.getText();
 		
-		boolean check = fDao.loginCheck(id, pwd);
+		boolean check = fDao.loginCheck1(id, pwd);
 		if(check) {
 			this.dispose();
-			new UserMenu(id);
+			String name = fDao.getName(id);
+			new UserMenu(name,id);
 		}else if(!check) {
 			this.IDtextField.setText("");
 			this.PWField.setText("");
