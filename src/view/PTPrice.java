66122fc8e22f7f33f9b1d9da6c4f751afb4d89ce
choi2864 <85035853+choi2864@users.noreply.github.com>
2021-10-19 15:53:39 +0900

    Add files via upload

--- src/view/PTPrice.java
+++ src/view/PTPrice.java
@@ -27,7 +27,7 @@ implements MouseListener, ActionListener {
    FitnessDao fDao;
    JButton previewBtn;
 
-   private int num = 0; // 횟수 저장해놓는 변수
+   private int num = 10; // 횟수 저장해놓는 변수
 
    public static void main(String[] args) {
       new PTPrice("1");
@@ -38,58 +38,59 @@ implements MouseListener, ActionListener {
       this.setTitle("남은 기간 연장");
 
       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      this.setSize(400, 460);
+      this.setSize(400, 440);
       this.setLocationRelativeTo(null);
       getContentPane().setLayout(null);
 
       JLabel month_1 = new JLabel("PT 10회");
       month_1.setHorizontalAlignment(SwingConstants.CENTER);
       month_1.setFont(new Font("굴림", Font.BOLD, 16));
-      month_1.setBounds(58, 35, 89, 30);
+      month_1.setBounds(58, 39, 89, 30);
       getContentPane().add(month_1);
 
       JLabel month_3 = new JLabel("PT 30회");
       month_3.setFont(new Font("굴림", Font.BOLD, 16));
       month_3.setHorizontalAlignment(SwingConstants.CENTER);
-      month_3.setBounds(58, 105, 89, 30);
+      month_3.setBounds(58, 109, 89, 30);
       getContentPane().add(month_3);
 
       JLabel month_6 = new JLabel("PT 60회");
       month_6.setHorizontalAlignment(SwingConstants.CENTER);
       month_6.setFont(new Font("굴림", Font.BOLD, 16));
-      month_6.setBounds(58, 175, 89, 30);
+      month_6.setBounds(58, 179, 89, 30);
       getContentPane().add(month_6);
 
       JLabel month_12 = new JLabel("PT 120회");
       month_12.setFont(new Font("굴림", Font.BOLD, 16));
       month_12.setHorizontalAlignment(SwingConstants.CENTER);
-      month_12.setBounds(58, 245, 89, 30);
+      month_12.setBounds(58, 249, 89, 30);
       getContentPane().add(month_12);
 
       extension = new JButton("연장하기");
-      extension.setBounds(106, 328, 157, 50);
+      extension.setFont(new Font("굴림", Font.PLAIN, 15));
+      extension.setBounds(106, 310, 157, 50);
       getContentPane().add(extension);
 
       bg = new ButtonGroup();
 
       w6 = new JRadioButton("500,000원");
       w6.setFont(new Font("굴림", Font.BOLD, 14));
-      w6.setBounds(204, 35, 121, 30);
+      w6.setBounds(204, 39, 121, 30);
       getContentPane().add(w6);
 
       w15 = new JRadioButton("1,000,000원");
       w15.setFont(new Font("굴림", Font.BOLD, 14));
-      w15.setBounds(204, 105, 121, 30);
+      w15.setBounds(204, 109, 121, 30);
       getContentPane().add(w15);
 
       w24 = new JRadioButton("2,400,000원");
       w24.setFont(new Font("굴림", Font.BOLD, 14));
-      w24.setBounds(204, 175, 121, 30);
+      w24.setBounds(204, 179, 121, 30);
       getContentPane().add(w24);
 
       w36 = new JRadioButton("3,600,000원");
       w36.setFont(new Font("굴림", Font.BOLD, 14));
-      w36.setBounds(204, 245, 121, 30);
+      w36.setBounds(204, 249, 121, 30);
       getContentPane().add(w36);
 
       bg.add(w6);
@@ -97,12 +98,14 @@ implements MouseListener, ActionListener {
       bg.add(w24);
       bg.add(w36);
       
-      previewBtn = new JButton("<이전");
+      previewBtn = new JButton("<회원화면");
       previewBtn.setFont(new Font("굴림", Font.PLAIN, 13));
-      previewBtn.setBounds(12, 10, 75, 23);
+      previewBtn.setBounds(12, 10, 97, 23);
       getContentPane().add(previewBtn);
 
    
+      w6.setSelected(true);
+      
       this.w6.addMouseListener(this);
       this.w15.addMouseListener(this);
       this.w24.addMouseListener(this);
@@ -166,8 +169,9 @@ implements MouseListener, ActionListener {
    @Override
    public void actionPerformed(ActionEvent e) {
       // TODO Auto-generated method stub
-	   if(e.getActionCommand().equals("<이전")) {
+	   if(e.getActionCommand().equals("<회원화면")) {
 		   this.dispose();
+		   new UserMenu(id);
 	   }
       fDao = new FitnessDao();
 
